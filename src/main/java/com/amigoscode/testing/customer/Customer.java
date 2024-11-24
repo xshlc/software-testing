738a3e51e90b9e3c7a41c418b43fe812114d1289
xshlc <heechuw@gmail.com>
2024-11-24 00:40:38 -0500

    Customer classes

---
+++ src/main/java/com/amigoscode/testing/customer/Customer.java
@@ -0,0 +1,44 @@
+package com.amigoscode.testing.customer;
+
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+public class Customer {
+    private UUID id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String phoneNumber;
+
+    public Customer(UUID id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/com/amigoscode/testing/customer/CustomerRegistrationService.java
@@ -0,0 +1,11 @@
+package com.amigoscode.testing.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerRegistrationService {
+
+    public void registerNewCustomer(CustomerRegistrationRequest request) {
+
+    }
+}
